1d80a08fbd2d9e481d5a066043ecad0a700978c3
premib <premib123@gmail.com>
2020-07-09 11:27:17 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpService } from "./http.service";
+import {} from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'PagedTable';
+  item: Array<any>= [];
+  page: number= 1;
+  constructor(private http:HttpService) {
+    this.getTableContents(this.page);
+  }
+  
+  getTableContents(pageNo: number){
+    this.http.getObjects(pageNo).subscribe(
+      data=>{
+        // console.log(data, this.page)
+        this.item= data
+      },
+      err=> console.log(err),
+      ()=> console.log("finne")
+    )
+  }
+
 }
---
+++ src/app/http.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from "rxjs";
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HttpService {
+
+  constructor(private http: HttpClient) { }
+
+  getObjects(pageNo: number): Observable<any>{
+    return this.http.get(`https://5f0404934c6a2b00164906a4.mockapi.io/users?page=${pageNo}&limit=10`);
+  }
+}
